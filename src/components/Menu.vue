bb113c3b8d0c8be3a3a650f3d3ea133942290e15
namnguyen161 <nam.nguyen@zestif.com>
2022-10-10 17:54:14 +0700

    first commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div v-show="isShow">
+            <input type="text" v-model="name">
+        </div>
+        <button @click="isShow = !isShow">{{isShow ? "Show" :"Hide"}}</button>
+       {{name}}
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Menu",
+    data() {
+        return {
+            name: "Nguyen Van Nam",
+            isShow: true
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
